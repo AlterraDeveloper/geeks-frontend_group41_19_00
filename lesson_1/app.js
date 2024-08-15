cd76bcbb53dc0864f4e15480a5a21a396cc21cb5
Alterra$Developer <42990792+AlterraDeveloper@users.noreply.github.com>
2024-08-15 21:11:33 +0600

    add lesson 1

---
+++ lesson_1/app.js
@@ -0,0 +1,57 @@
+
+// alert("Hello group 41 from APP.JS!");
+
+// V8 - JavaScript Engine
+
+// variables - переменные
+
+// var let const 
+
+let myName = "Evgeniy";
+let mySurname = "Kiselev";
+let myFatherName = "Dmitrievich";
+
+console.log("My full name is: " + mySurname + " " + myName + " " + myFatherName);
+
+let myAge = 18;
+console.log("In next year my age will be " + (myAge + 1));
+
+// Data types - типы данных
+// MDN - Mozilla Developer Network
+
+// 1. string - текстовый
+// 2. number - числовой
+// 3. boolean - логический
+// 4. undefined - неопределенный
+// 5. null - пустой
+// 6. object - объект
+// 7. symbol
+// 8. bigint
+
+let producerName = "Xiaomi";
+let modelName = "Redmi 14S MAX";
+let osName = 'Android';
+
+console.log("My smartphone is " + producerName + " " + modelName 
+    + "(" + osName + ")");
+
+let cpuCores = 4;
+let ramGb = 6;
+let storageGb = 256;
+let price = 5_000_000;
+let displaySize = 10.28;
+
+console.log("It's price on black friday is " + price / 2);
+
+let supportsNFC = true; // 0
+let supports3G = true; // 1
+let supports4G = true;
+let supports5G = false;
+let supportQuickCharge = true;
+
+console.log("My smartphone use NFC " + (supportsNFC ? "YES" : "NO"));
+
+
+
+
+
